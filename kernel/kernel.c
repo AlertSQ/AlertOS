5043f9e9ea423c3b56c0a19f3e36a639c8124541
fbigovwant <huysosi@mail.com>
2025-01-08 20:39:21 +0700

    syscalls spliting

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,5 +9,5 @@ void kmain(void)
   init_idt();
   vga_print("Ascertain kernel\n");
   idt_set_descriptor(0x80, dispather, 0xEE); /* Load syscall dispather to IDT */
-  __asm__ __volatile__("movl $1, %eax; int $0x80"); /* Testing syscall */
+  __asm__ __volatile__("movl $0, %eax; int $0x80"); /* Testing syscall */
 }
